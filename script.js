fa15529b37a24c306aee53b0c4e0bf40099575db
Marvin Reyes <marvinclr@gmail.com>
2026-03-02 23:36:00 +0800

    Final output

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+const form = document.getElementById('form');
+form.addEventListener('submit', confirmPassword);
+
+function confirmPassword(e) {
+    e.preventDefault();
+    const password = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    const message = document.getElementById('error-message')
+
+    if (password.value.length > 0) {
+        if(password.value !== confirmPasswordInput.value) {
+           message.textContent = "Password does not match.";
+        } else {
+            message.textContent = "Registered Successfully";
+        }
+    } else {
+        message.textContent = 'Please input a password.';
+    }
+}
+
+function resetForm(){
+    document.getElementById("form").reset();
+}
